60460d06c8211664d2e3593e264540c7aff8fc95
ankur1812 <ankur.anmol1992@gmail.com>
2024-07-31 20:15:34 +0200

    header / footer layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app-layout">
+      <header>
+        <h2>Advent of Code '24</h2>
+      </header>
+      
+      <main>
+        <NuxtPage />
+      </main>
+      
+      <!--
+      <footer>
+        <p>Footer</p>
+      </footer>
+      -->
+    </div>
+  </template>
+    
+<script setup>
+    // definePageMeta({
+    //     layout: 'custom' // This would use layouts/custom.vue if it exists
+    // })
+</script>
