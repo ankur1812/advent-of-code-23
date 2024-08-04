04b6cda6c5c2d36be6ee7a94304da49cddcac752
ankur1812 <ankur.anmol1992@gmail.com>
2024-08-04 22:52:13 +0200

    homepage updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,54 @@
 <template>
-    <div>
-      <h1>Home Page</h1>
+  <div class="home">
+    <div class="header2">
+      <h1 class="title">Code Solver</h1>
+      <h2 class="subtitle">Solutions + Playground for Advent of Code 2023</h2>
+      <!-- <h2 class="subtitle">A Daily Coding Challenge Adventure</h2> -->
     </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
+
+    <br />
+    <section class="intro">
+      <p>
+        Welcome to Code Solver. This app is designed for solving the daily
+        problems on Advent of Code '23.
+      </p>
+      <p>
+        Advent of Code is an annual coding challenge that takes place every
+        December. It offers a series of engaging and progressively challenging
+        puzzles that span from December 1st to December 25th. Whether you're a
+        seasoned developer or a beginner, Advent of Code is a fantastic way to
+        test your problem-solving skills and explore new programming concepts.
+      </p>
+      <p>
+        Each day brings a new puzzle with two parts, designed to be tackled in
+        sequence. The problems are inspired by a wide range of topics and
+        encourage creative solutions.
+      </p>
+      <p>
+        To find out more about the challanges, visit the
+        <a
+          href="https://adventofcode.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          >official Advent of Code website</a
+        >.
+      </p>
+    </section>
+    <br />
+    <section class="cta">
+      <h2>Solve the challenges here</h2>
+      <p>
+        You can solve the daily problems for Advent of Code '2023 here directly.
+        Start with
+        <a href="/day/1" rel="noopener noreferrer" class="cta-button">Day 1</a>
+      </p>
+    </section>
+  </div>
+</template>
+<script setup></script>
+
+<style scoped>
+.home {
+  padding: 0 4rem;
+}
+</style>
